6225f890a9624ed0d16a3b907c76914d89348f80
briefdog <121690859+briefdog@users.noreply.github.com>
2023-10-07 22:26:16 +0100

    loginpage.py

--- loginpage.py
+++ loginpage.py
@@ -31,35 +31,42 @@ window.configure(bg="aqua")
 #uses register entries as variables
 username = StringVar()
 password = StringVar()
+deleteaccount_username = StringVar()
 
 
 #adds username and hashed password to database
 def registerinfo():
     username_info = username.get()
     password_info = password.get()
-    username_info = username_info.lower()
-    h = hashlib.sha256()
-    h.update(password_info.encode("utf-8"))
-    hashed_password = h.hexdigest()
-    cur.execute("INSERT INTO users (username, password) VALUES (?,?)", (username_info, hashed_password))
-    conn.commit()
+    usernamelength = len(username_info)
+    passwordlength = len(password_info)
+    if (usernamelength >= 5) and (passwordlength >= 5):
+        username_info = username_info.lower()
+        h = hashlib.sha256()
+        h.update(password_info.encode("utf-8"))
+        hashed_password = h.hexdigest()
+        cur.execute("INSERT INTO users (username, password) VALUES (?,?)", (username_info, hashed_password))
+        conn.commit()
+    else:
+        messagebox.showerror(title="Error", message="Username and password must be 5 characters long")
     newusername_entry.delete(0, END)
     newpassword_entry.delete(0, END)
+
 
 #uses login entries as variables
 username_by_user = StringVar()
 password_by_user = StringVar()
 
 def login_correct():
-    messagebox.showinfo(title="Login Success", message="You successfully logged in.")
+    messagebox.showinfo(title="Login Success", message="You successfully logged in")
     call(["python","homepage.py"])
     window.destroy()
 
 def login_wrong():
-    messagebox.showerror(title="Error", message="Invalid password.")
+    messagebox.showerror(title="Error", message="Invalid password")
 
 def user_not_found():
-    messagebox.showerror(title="Error", message="Invalid username.")
+    messagebox.showerror(title="Error", message="Invalid username")
 
 #compares user input with username and hashed password database
 
@@ -86,11 +93,16 @@ def login():
     username_entry.delete(0, END)
     password_entry.delete(0, END)
     
+# deletes an account
+
 def delete_acccount():
-    pass
+    delete_username = deleteaccount_username.get()
+    delete_username = delete_username.lower()
+    cur.execute("DELETE FROM users WHERE username = ?",(delete_username,))
+    conn.commit()
+    deleteaccount_entry.delete(0, END)
+    deleteaccount_entry.delete(0, END)
     
-
-
 
 #create widgets for login
 
@@ -111,8 +123,8 @@ newusername_entry = tk.Entry(textvariable = username)
 newpassword_entry = tk.Entry(textvariable = password)
 newpassword_label = tk.Label( text="New Password", bg = "aqua", font="Arial")
 register_button = tk.Button(text="register", command = registerinfo)
-delete_button = tk.Button(text = "delete account")
-deleteaccount_entry = tk.Entry()
+delete_button = tk.Button(text = "delete account", command = delete_acccount)
+deleteaccount_entry = tk.Entry(textvariable= deleteaccount_username)
 deleteaccount_label = tk.Label(text = "Delete Account",bg= "aqua", font="Arial")
 
 #place widgets manually for login
@@ -137,10 +149,7 @@ delete_button.place(x = 300, y=300 )
 deleteaccount_entry.place(x=315, y=270)
 deleteaccount_label.place(x=180, y=265)
 
-
-
 #run window
-
 
 window.mainloop()
 
