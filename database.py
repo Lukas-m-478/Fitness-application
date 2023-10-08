79ba59b2a0af5b084da3b4f26649142b582826d6
briefdog <121690859+briefdog@users.noreply.github.com>
2023-10-08 20:54:11 +0100

    database.py

--- database.py
+++ database.py
@@ -1,13 +1,13 @@
 #creates database
 import sqlite3
-conn = sqlite3.connect("logindetails.db")
+conn = sqlite3.connect("information.db")
 cur = conn.cursor()
 
 #creates table with login details if it does not exist
 cur.execute("""
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE, -- UNIQUE constraint
     password VARCHAR(255) NOT NULL       
 )            
 """)
