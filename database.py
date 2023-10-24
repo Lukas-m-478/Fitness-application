2ebe674b48d6535661879ec601fcc545e45ef44d
briefdog <121690859+briefdog@users.noreply.github.com>
2023-10-24 18:24:40 +0100

    database.py

--- database.py
+++ database.py
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS upper_body_exercises (
 cur.execute("""
 CREATE TABLE IF NOT EXISTS lower_body_exercises (
     id INTEGER PRIMARY KEY,
-    exercise VARCHAR(255) NOT NULL      
+    exercise VARCHAR(255) NOT NULL   
 )            
 """)
 
@@ -53,24 +53,17 @@ CREATE TABLE IF NOT EXISTS cardio (
 """)
 
 #insert upper body exercises
-cur.execute("INSERT INTO upper_body_exercises (exercise) VALUES (?)", ("push ups",))
 cur.execute("INSERT INTO upper_body_exercises (exercise) VALUES (?)", ("bicep curls",))
 cur.execute("INSERT INTO upper_body_exercises (exercise) VALUES (?)", ("hammer curls",))
 cur.execute("INSERT INTO upper_body_exercises (exercise) VALUES (?)", ("preacher curls",))
 cur.execute("INSERT INTO upper_body_exercises (exercise) VALUES (?)", ("barbell curls",))
-cur.execute("INSERT INTO upper_body_exercises (exercise) VALUES (?)", ("shoulder press",))
 cur.execute("INSERT INTO upper_body_exercises (exercise) VALUES (?)", ("lateral raise",))
-cur.execute("INSERT INTO upper_body_exercises (exercise) VALUES (?)", ("lateral pull-down",))
-cur.execute("INSERT INTO upper_body_exercises (exercise) VALUES (?)", ("rows",))
 cur.execute("INSERT INTO upper_body_exercises (exercise) VALUES (?)", ("tricep extensions",))
 cur.execute("INSERT INTO upper_body_exercises (exercise) VALUES (?)", ("rear delts",))
-cur.execute("INSERT INTO upper_body_exercises (exercise) VALUES (?)", ("pec fly",))
 
 #insert lower body exercises
 cur.execute("INSERT INTO lower_body_exercises (exercise) VALUES (?)", ("leg extensions",))
-cur.execute("INSERT INTO lower_body_exercises (exercise) VALUES (?)", ("leg press",))
 cur.execute("INSERT INTO lower_body_exercises (exercise) VALUES (?)", ("leg curls",))
-cur.execute("INSERT INTO lower_body_exercises (exercise) VALUES (?)", ("lunges",))
 cur.execute("INSERT INTO lower_body_exercises (exercise) VALUES (?)", ("calf raises",))
 
 #insert compound exercises
@@ -79,7 +72,13 @@ cur.execute("INSERT INTO compound_exercises (exercise) VALUES (?)", ("dips",))
 cur.execute("INSERT INTO compound_exercises (exercise) VALUES (?)", ("squats",))
 cur.execute("INSERT INTO compound_exercises (exercise) VALUES (?)", ("deadlift",))
 cur.execute("INSERT INTO compound_exercises (exercise) VALUES (?)", ("bench press",))
-
+cur.execute("INSERT INTO compound_exercises (exercise) VALUES (?)", ("push ups",))
+cur.execute("INSERT INTO compound_exercises (exercise) VALUES (?)", ("shoulder press",))
+cur.execute("INSERT INTO compound_exercises (exercise) VALUES (?)", ("lateral pull-down",))
+cur.execute("INSERT INTO compound_exercises (exercise) VALUES (?)", ("pec fly",))
+cur.execute("INSERT INTO compound_exercises (exercise) VALUES (?)", ("rows",))
+cur.execute("INSERT INTO compound_exercises (exercise) VALUES (?)", ("leg press",))
+cur.execute("INSERT INTO compound_exercises (exercise) VALUES (?)", ("lunges",))
 
 #insert cardio exercises
 cur.execute("INSERT INTO cardio (exercise) VALUES (?)", ("running",))
@@ -94,6 +93,57 @@ cur.execute("INSERT INTO core_exercises (exercise) VALUES (?)", ("sit ups",))
 cur.execute("INSERT INTO core_exercises (exercise) VALUES (?)", ("plank",))
 cur.execute("INSERT INTO core_exercises (exercise) VALUES (?)", ("hanging leg raise",))
 cur.execute("INSERT INTO core_exercises (exercise) VALUES (?)", ("hollow hold",))
+
+#add exercise_day column to upper and lower body exercise tables (it stores "Push", "Pull" or "Legs")
+#add exercise_days column and target_muscles column to compound exercise table
+cur.execute("ALTER TABLE upper_body_exercises ADD COLUMN exercise_day VARCHAR(255)")
+cur.execute("ALTER TABLE lower_body_exercises ADD COLUMN exercise_day VARCHAR(255)")
+cur.execute("ALTER TABLE compound_exercises ADD COLUMN exercise_day VARCHAR(255)")
+cur.execute("ALTER TABLE compound_exercises ADD COLUMN target_muscles VARCHAR(255)")
+cur.execute("ALTER TABLE upper_body_exercises ADD COLUMN target_muscle VARCHAR(255)")
+cur.execute("ALTER TABLE lower_body_exercises ADD COLUMN target_muscle VARCHAR(255)")
+
+#add "Push", "Pull" or "Legs" to upper body exercises table to specify what exercise fits where
+cur.execute("UPDATE upper_body_exercises SET exercise_day = ? WHERE exercise = ?", ("Pull", "bicep curls"))
+cur.execute("UPDATE upper_body_exercises SET exercise_day = ? WHERE exercise = ?", ("Pull", "hammer curls"))
+cur.execute("UPDATE upper_body_exercises SET exercise_day = ? WHERE exercise = ?", ("Pull", "preacher curls"))
+cur.execute("UPDATE upper_body_exercises SET exercise_day = ? WHERE exercise = ?", ("Pull", "barbell curls"))
+cur.execute("UPDATE upper_body_exercises SET exercise_day = ? WHERE exercise = ?", ("Push", "lateral raise"))
+cur.execute("UPDATE upper_body_exercises SET exercise_day = ? WHERE exercise = ?", ("Push", "tricep extensions"))
+cur.execute("UPDATE upper_body_exercises SET exercise_day = ? WHERE exercise = ?", ("Push", "rear delts"))
+
+#add "Push", "Pull" or "Legs" to lower body exercises table to specify what exercise fits where
+cur.execute("UPDATE lower_body_exercises SET exercise_day = ? WHERE exercise = ?", ("Legs", "leg extensions"))
+cur.execute("UPDATE lower_body_exercises SET exercise_day = ? WHERE exercise = ?", ("Legs", "leg curls"))
+cur.execute("UPDATE lower_body_exercises SET exercise_day = ? WHERE exercise = ?", ("Legs", "calf raises"))
+
+#add "Push", "Pull" or "Legs" to compound exercises table to specify what exercise fits where
+cur.execute("UPDATE compound_exercises SET exercise_day = ? WHERE exercise = ?", ("Pull", "pull ups"))
+cur.execute("UPDATE compound_exercises SET exercise_day = ? WHERE exercise = ?", ("Push", "dips"))
+cur.execute("UPDATE compound_exercises SET exercise_day = ? WHERE exercise = ?", ("Legs", "squats"))
+cur.execute("UPDATE compound_exercises SET exercise_day = ? WHERE exercise = ?", ("Legs,Pull", "deadlift"))
+cur.execute("UPDATE compound_exercises SET exercise_day = ? WHERE exercise = ?", ("Push", "bench press"))
+cur.execute("UPDATE compound_exercises SET exercise_day = ? WHERE exercise = ?", ("Push", "push ups"))
+cur.execute("UPDATE compound_exercises SET exercise_day = ? WHERE exercise = ?", ("Push", "shoulder press"))
+cur.execute("UPDATE compound_exercises SET exercise_day = ? WHERE exercise = ?", ("Pull", "lateral pull-down"))
+cur.execute("UPDATE compound_exercises SET exercise_day = ? WHERE exercise = ?", ("Push", "pec fly"))
+cur.execute("UPDATE compound_exercises SET exercise_day = ? WHERE exercise = ?", ("Pull", "rows"))
+cur.execute("UPDATE compound_exercises SET exercise_day = ? WHERE exercise = ?", ("Legs", "leg press"))
+cur.execute("UPDATE compound_exercises SET exercise_day = ? WHERE exercise = ?", ("Legs", "lunges"))
+
+#add muscle name to target_muscle column in upper body exercise table to specify what muscle each exercise works
+cur.execute("UPDATE upper_body_exercises SET target_muscle = ? WHERE exercise = ?", ("biceps", "bicep curls"))
+cur.execute("UPDATE upper_body_exercises SET target_muscle = ? WHERE exercise = ?", ("biceps", "hammer curls"))
+cur.execute("UPDATE upper_body_exercises SET target_muscle = ? WHERE exercise = ?", ("biceps", "preacher curls"))
+cur.execute("UPDATE upper_body_exercises SET target_muscle = ? WHERE exercise = ?", ("biceps", "barbell curls"))
+cur.execute("UPDATE upper_body_exercises SET target_muscle = ? WHERE exercise = ?", ("lateral delts", "lateral raise"))
+cur.execute("UPDATE upper_body_exercises SET target_muscle = ? WHERE exercise = ?", ("triceps", "tricep extensions"))
+cur.execute("UPDATE upper_body_exercises SET target_muscle = ? WHERE exercise = ?", ("shoulders", "rear delts"))
+
+#add muscle name to target_muscle column in lower body exercise table to specify what muscle each exercise works
+cur.execute("UPDATE lower_body_exercises SET target_muscle = ? WHERE exercise = ?", ("quads", "leg extensions"))
+cur.execute("UPDATE lower_body_exercises SET target_muscle = ? WHERE exercise = ?", ("hamstrings", "leg curls"))
+cur.execute("UPDATE lower_body_exercises SET target_muscle = ? WHERE exercise = ?", ("calves", "calf raises"))
 
 #execute queries
 conn.commit()
